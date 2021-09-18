728c4c27272ed179788ac33b62d9172c31b5d851
kytosai <kytosai@gmail.com>
2021-09-19 02:20:38 +0700

    update document

--- src/behavioralPatterns/strategyPattern/Ticket.ts
+++ src/behavioralPatterns/strategyPattern/Ticket.ts
@@ -36,6 +36,19 @@ export class Ticket {
   }
 
   getPromotedPrice() {
+    /*
+      Chỗ này có 1 lưu ý là khi demo bằng C# thì không cần set giá trị
+      khởi tạo cho trường hợp prop #promoteStrategy, tuy nhiên bên ts
+      nếu không set thì sẽ là undefined thì khi cho vào `doDiscount()`
+      chắc chắn sẽ bị lỗi:
+
+      -> Nếu vậy thì việc set giá trị là `0` khi #price bị undefined
+      liệu có đúng đắng không ?
+
+      -> Nếu vậy trước khi `doDiscount()` ta phải check `#promoteStrategy`
+      để tránh undefined nhưng vô tình làm return về có kiểu `undefined`
+      liệu hàm này có còn đúng đắn hay không ?
+    */
     return this.#promoteStrategy?.doDiscount(this.#price ?? 0);
   }
 }
